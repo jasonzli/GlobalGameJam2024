fee50c49f34c6d88229defac4cbe372af46e81f1
Luke Kim <kiml803@newschool.edu>
2024-01-27 12:14:28 -0500

    Organized files

--- Assets/PlayerController/Player Scripts/RotateCameraSmooth.cs
+++ Assets/PlayerController/Player Scripts/RotateCameraSmooth.cs
@@ -4,35 +4,36 @@ using UnityEngine;
 
 public class RotateCameraSmooth : MonoBehaviour
 {
-    public float rotationSpeed = 5f; // Adjust the rotation speed as needed
+    public float rotationSpeed = 5f; 
 
     private bool isRotating = false;
-    private Quaternion originalRotation;
-    private Quaternion targetRotation;
-
-    void Start()
-    {
-        originalRotation = transform.rotation;
-        targetRotation = Quaternion.Euler(0f, -170f, 0f) * originalRotation;
-    }
+    private Vector3 targetDirection;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            // Right mouse button pressed, start rotating
-            isRotating = true;
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                targetDirection = (hit.point - transform.position).normalized;
+                isRotating = true;
+            }
         }
 
         if (Input.GetMouseButtonUp(1))
         {
-            // Right mouse button released, stop rotating
+           
             isRotating = false;
         }
 
         if (isRotating)
         {
-            // Smoothly rotate towards the target rotation
+           
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
     }
