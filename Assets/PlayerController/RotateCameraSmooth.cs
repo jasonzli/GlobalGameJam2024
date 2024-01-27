20d7ff2e8bf9ede0a5bb083276c0277408b5f0f3
Luke Kim <kiml803@newschool.edu>
2024-01-27 11:20:29 -0500

    Updated Player with Arm + Fixed Grab Animation

--- Assets/PlayerController/RotateCameraSmooth.cs
+++ Assets/PlayerController/RotateCameraSmooth.cs
@@ -4,42 +4,36 @@ using UnityEngine;
 
 public class RotateCameraSmooth : MonoBehaviour
 {
-    public float rotationSpeed = 5f; 
+    public float rotationSpeed = 5f; // Adjust the rotation speed as needed
 
     private bool isRotating = false;
     private Quaternion originalRotation;
+    private Quaternion targetRotation;
 
     void Start()
     {
         originalRotation = transform.rotation;
+        targetRotation = Quaternion.Euler(0f, -170f, 0f) * originalRotation;
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-           
+            // Right mouse button pressed, start rotating
             isRotating = true;
         }
 
         if (Input.GetMouseButtonUp(1))
         {
-           
+            // Right mouse button released, stop rotating
             isRotating = false;
         }
 
         if (isRotating)
         {
-           
-            Quaternion targetRotation = originalRotation * Quaternion.Euler(0f, -170f, 0f);
-
-           
+            // Smoothly rotate towards the target rotation
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
-        else
-        {
-           
-            transform.rotation = Quaternion.Lerp(transform.rotation, originalRotation, rotationSpeed * Time.deltaTime);
         }
     }
 }
