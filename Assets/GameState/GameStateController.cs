cae7da5f80d81a42f88ee9902c5abf28c59f8482
Jason <jasonli1337@gmail.com>
2024-01-28 01:09:24 -0500

    Create the GameStateController component

---
+++ Assets/GameState/GameStateController.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateController : MonoBehaviour
+{
+    // This class needs to be able to start the game, reset the game and also keep track of the current state of the game
+
+    #region Assignable Fields for the Main Scene
+    [SerializeField] private Transform _playerSpawnPoint;
+    [SerializeField] private Transform _canvasRoot;
+    [SerializeField] private Camera _titleCamera;
+    #endregion
+    
+    #region Game State Properties
+
+    public Action<int> BananaScoreChanged;
+    public Action<int> BananaHitScoreChanged;
+    public Action<int> MonkeysSurvivedChanged;
+
+    private int _bananasEaten;
+    public int BananasEaten
+    {
+        get => _bananasEaten;
+        private set
+        {
+            BananaScoreChanged?.Invoke(value);
+            _bananasEaten = value;
+        }
+    }
+
+    private int _bananasHit;
+    public int BananasHit
+    {
+        get => _bananasHit;
+        private set
+        {
+            BananaHitScoreChanged?.Invoke(value);
+            _bananasHit = value;
+        }
+    }
+    private int _monkeysSurvived;
+
+    public int MonkeysSurvived
+    {
+        get => _monkeysSurvived;
+        private set
+        {
+            MonkeysSurvivedChanged?.Invoke(value);
+            _monkeysSurvived = value;
+        }
+    }
+
+    public int SecondsSurvived { get; private set; }
+    #endregion
+    
+    public Action OnGameStart;
+    public Action OnGameReset;
+    public Action OnGameEnd;
+
+    public enum GameState
+    {
+        Title,
+        Playing,
+        End
+    }
+
+    private GameState _gameState;
+    public GameState CurrentGameState
+    {
+        get => _gameState;
+        private set => _gameState = value;
+    }
+    
+    #region Private References
+
+    private PlayerController _activePlayer;
+    private TitleScreenUI _titleScreenUI;
+    private PlayerUI _playerUI;
+    private EndScreenUI _endScreenUI;
+    #endregion
+
+    public void ShowTitleScreen()
+    {
+        if (_titleScreenUI == null)
+        {
+            _titleScreenUI = GameManager.Instance.CreateInstance<TitleScreenUI>(_canvasRoot);
+            _titleScreenUI.startButton.onClick.AddListener(StartGame);
+            _titleScreenUI.quitButton.onClick.AddListener(QuitGame);
+        }
+        
+        _titleScreenUI.gameObject.SetActive(true);
+        // Set the camera to the title screen camera
+        _titleCamera.gameObject.SetActive(true);
+        _titleCamera.tag = "MainCamera";
+        _activePlayer.CameraComponent.tag = "Untagged";
+        
+        CurrentGameState = GameState.Title;
+    }
+
+    private void AddScore()
+    {
+        BananasEaten++;
+    }
+    
+    private void AddHitScore()
+    {
+        BananasHit++;
+    }
+    
+    public void StartGame()
+    {
+        // Turn off the title scree
+        _titleScreenUI.gameObject.SetActive(false);
+        
+        // Spawn or reset the player
+        if (_activePlayer == null)
+        {
+            _activePlayer = GameManager.Instance.CreateInstance<PlayerController>(null, _playerSpawnPoint.position, _playerSpawnPoint.rotation);
+            _activePlayer.OnBananaEaten += AddScore;
+        }
+        else
+        {
+            _activePlayer.transform.position = _playerSpawnPoint.position;
+            _activePlayer.transform.rotation = _playerSpawnPoint.rotation;
+        }
+        
+        // Change the main Camera to the one on the PlayerCharacter instance
+        _titleCamera.tag = "Untagged";
+        _titleCamera.gameObject.SetActive(false);
+        _activePlayer.CameraComponent.tag = "MainCamera";
+        
+        // Reset score values
+        BananasEaten = 0;
+        BananasHit = 0;
+        MonkeysSurvived = 0;
+        SecondsSurvived = 0;
+        
+        // Monkeys already spawned
+        // Nothing to do for now
+        
+        // Create the Player UI system
+        if (_playerUI == null)
+        {
+            _playerUI = GameManager.Instance.CreateInstance<PlayerUI>(_canvasRoot);
+        }
+
+        _playerUI.SetToState(this);
+        
+        OnGameStart?.Invoke();
+        // Set the game state to playing
+        CurrentGameState = GameState.Playing;
+    }
+
+    public void EndGame()
+    {
+        // Deactivate the player
+        _activePlayer.gameObject.SetActive(false);
+        
+
+        ShowEndScreen();
+
+        OnGameEnd?.Invoke();
+        CurrentGameState = GameState.End;
+    }
+    
+    public void ShowEndScreen()
+    {
+        if (_endScreenUI == null)
+        {
+            _endScreenUI = GameManager.Instance.CreateInstance<EndScreenUI>(_canvasRoot);
+            _endScreenUI.resetButton.onClick.AddListener(SetupGame);
+            _endScreenUI.quitButton.onClick.AddListener(QuitGame);
+        }
+        _endScreenUI.gameObject.SetActive(true);
+        _endScreenUI.SetToState(this);
+    }
+
+    public void SetupGame()
+    {
+        // TODO something to do with all the monkeys so they can be reset too.
+        
+        OnGameReset?.Invoke();
+        ShowTitleScreen();
+    }
+    
+    private void QuitGame()
+    {
+#if !UNITY_STANDALONE
+        Application.Quit();
+#endif
+    }
+}
